79521800359e7dfee1dd52536d728219025741ba
saveetha90 <saveethakathiravan@gmail.com>
2021-04-14 16:10:47 -0400

    fixed login issue

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
     System.out.println("Hello World");
-        System.out.println("Hello Savee");
-        System.out.println("Hello Sudha");
+                System.out.println("Hello Sudha");
  }
 
 }
